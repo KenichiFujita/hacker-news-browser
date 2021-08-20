68dcf2ace66ea686af7912e15e0a289b18d81fec
Ken <ken.fujita1130@icloud.com>
2021-08-20 10:14:07 -0400

    Rewrite StoriesViewModel and StoriesViewController to adopt inputs/outputs patter

--- HackerNews/ViewModels/StoriesViewModel.swift
+++ HackerNews/ViewModels/StoriesViewModel.swift
@@ -7,39 +7,59 @@
 //
 
 import UIKit
+import SafariServices
 
-protocol StoriesViewModelDelegate: class {
-    func storiesViewModelUpdated(_ viewModel: StoriesViewModelType)
+protocol StoriesViewModelType: AnyObject {
+    var inputs: StoriesViewModelInputs { get }
+    var outputs: StoriesViewModelOutputs { get }
 }
 
-protocol StoriesViewModelType: class {
-    var stories: [Story] { get }
-    var hasMore: Bool { get }
-    var delegate: StoriesViewModelDelegate? { get set }
-    var canShowInstruction: Bool { get }
-    var favoritesStore: FavoritesStore { get }
+protocol StoriesViewModelInputs {
     func viewDidLoad()
     func didPullToRefresh()
     func lastCellWillDisplay()
+    func storyCellCommentButtonTapped(at indexPath: IndexPath)
+    func didSelectRowAt(_ indexPath: IndexPath)
+}
+
+ protocol StoriesViewModelOutputs: AnyObject {
+    var stories: [Story] { get }
+    var reloadData: () -> Void { get set }
+    var didReceiveServiceError: (Error) -> Void { get set }
+    var pushViewController: (StoryViewController) -> Void { get set }
+    var presentViewController: (UIViewController) -> Void { get set }
 }
 
 
-class StoriesViewModel: StoriesViewModelType {
+class StoriesViewModel: StoriesViewModelType, StoriesViewModelOutputs {
 
-    private(set) var stories: [Story] = [] {
+    var inputs: StoriesViewModelInputs { return self }
+
+    var outputs: StoriesViewModelOutputs { return self }
+
+    var stories: [Story] = [] {
         didSet {
-            delegate?.storiesViewModelUpdated(self)
+            reloadData()
         }
     }
-    weak var delegate: StoriesViewModelDelegate?
-    let store: StoryStore
+
+    var reloadData: () -> Void = { }
+
+    var didReceiveServiceError: (Error) -> Void = { _ in }
+
+    var pushViewController: (StoryViewController) -> Void = { _ in }
+
+    var presentViewController: (UIViewController) -> Void = { _ in }
+
     let storyImageInfoStore: StoryImageInfoStore
-    let type: StoryQueryType
-    var hasMore: Bool = false
-    var canShowInstruction: Bool {
-      return false
-    }
-    var favoritesStore: FavoritesStore
+
+    private let store: StoryStore
+
+    private let type: StoryQueryType
+
+    private var hasMore: Bool = false
+
+    private var favoritesStore: FavoritesStore
 
     init(storyQueryType type: StoryQueryType, storyStore: StoryStore, storyImageInfoStore: StoryImageInfoStore, favoritesStore: FavoritesStore) {
         self.type = type
@@ -47,6 +67,28 @@ class StoriesViewModel: StoriesViewModelType {
         self.storyImageInfoStore = storyImageInfoStore
         self.favoritesStore = favoritesStore
     }
+
+    private func load(offset: Int = 0) {
+        hasMore = false
+        store.stories(for: self.type, offset: offset, limit: 10) { [weak self] (result) in
+            guard let strongSelf = self else { return }
+            switch result {
+            case .success(let stories):
+                if offset == 0 {
+                    strongSelf.stories = stories
+                } else {
+                    strongSelf.stories.append(contentsOf: stories)
+                }
+                strongSelf.hasMore = stories.count == 10 ? true : false
+            case .failure(let error):
+                strongSelf.didReceiveServiceError(error)
+            }
+        }
+    }
+
+}
+
+extension StoriesViewModel: StoriesViewModelInputs {
 
     func viewDidLoad() {
         load()
@@ -59,28 +101,22 @@ class StoriesViewModel: StoriesViewModelType {
     }
 
     func lastCellWillDisplay() {
-        load(offset: stories.count)
-    }
-
-    private func load(offset: Int = 0) {
-        hasMore = false
-        store.stories(for: self.type,
-                      offset: offset,
-                      limit: 10) { [weak self] (result) in
-            guard let strongSelf = self else { return }
-            if case .success(let stories) = result {
-                if offset == 0 {
-                    strongSelf.stories = stories
-                } else {
-                    strongSelf.stories.append(contentsOf: stories)
-                }
-                if stories.count < 10 {
-                    strongSelf.hasMore = false
-                } else {
-                    strongSelf.hasMore = true
-                }
-            }
+        if hasMore {
+            load(offset: stories.count)
         }
     }
 
+    func storyCellCommentButtonTapped(at indexPath: IndexPath) {
+        pushViewController(StoryViewController(story: stories[indexPath.row], favoritesStore: favoritesStore))
+    }
+
+    func didSelectRowAt(_ indexPath: IndexPath) {
+        let story = stories[indexPath.row]
+        if let url = story.url, let url = URL(string: url) {
+            presentViewController(SFSafariViewController(url: url))
+        } else {
+            pushViewController(StoryViewController(story: story, favoritesStore: favoritesStore))
+        }
+    }
+    
 }
